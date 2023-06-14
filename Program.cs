e8a8a68d3e9a7ca87a0dddd9d2e505f3f1e14448
Bogdan <killah898993@gmail.com>
2023-06-14 21:58:05 +0500

    Homework

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+// Console.WriteLine("Input first number: ");
+// int num1 = Convert.ToInt32 (Console.ReadLine());
+
+
+// Console.WriteLine("Input second number: ");
+// int num2 = Convert.ToInt32 (Console.ReadLine());
+
+// if (num1 > num2)
+
+//  Console.WriteLine("max=" + num1);
+
+// else 
+
+//  Console.WriteLine("max=" + num2);
+
+ 
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+// Console.WriteLine("Input first num: ");
+// int num1=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input second num: ");
+// int num2=Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input third num: ");
+// int num3=Convert.ToInt32(Console.ReadLine());
+
+// int MaxNum = num1;
+
+// // if (MaxNum<num1)
+// //     MaxNum=num1;
+
+// if (MaxNum<num2)
+//     MaxNum=num2;
+
+// if (MaxNum<num3)
+//     MaxNum=num3;
+
+// Console.WriteLine("Максимальное число "+MaxNum);
+
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// Console.WriteLine("Да");
+
+// else 
+// Console.WriteLine("Нет");
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+// for (int current = 1; current<=num; current++)
+// {
+//     if ( current % 2 == 0 )
+//     Console.Write(current+ " ");
+// }
+////
